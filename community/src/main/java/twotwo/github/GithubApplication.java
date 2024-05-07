ef1e56911caf58c2f11475e6143d2d3028854470
ddolly518 <20211109@sungshin.ac.kr>
2024-05-07 18:32:50 +0900

    fix : Application.java

--- community/src/main/java/twotwo/github/GithubApplication.java
+++ community/src/main/java/twotwo/github/GithubApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class CommunityApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(CommunityApplication.class, args);
 	}
 
