18ea3edfef32d311ec52518e374e445604f2b4a1
ddolly518 <20211109@sungshin.ac.kr>
2024-05-22 10:45:34 +0900

    feat : add GithubApi,Level,CommitHistory

---
+++ github/src/main/java/twotwo/github/CommitHistory.java
@@ -0,0 +1,41 @@
+package zerobase.bud.domain;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@SuperBuilder
+@Entity
+public class CommitHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private GithubInfo githubInfo;
+
+    private long commitCount;
+
+    private long consecutiveCommitDays;
+
+    private LocalDate commitDate;
+
+    public void updateCommitCount(Long commitCount) {
+        this.commitCount = commitCount;
+    }
+}
